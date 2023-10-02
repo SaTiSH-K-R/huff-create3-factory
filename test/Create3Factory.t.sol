adabda4d21d9efef7d5fd1bb23ebd76138328315
SaTiSH-K-R <satishr1825@gmail.com>
2023-10-02 22:29:03 +0530

    tests solved

--- test/Create3Factory.t.sol
+++ test/Create3Factory.t.sol
@@ -19,8 +19,8 @@ contract Create3FactoryTest is Test {
     /// @dev Address of the Create3Factory contract.
     Create3Factory public factory;
 
-    bytes SimpleStoreBytecode = bytes(abi.encodePacked(0x602d8060093d393df35f3560e01c8063552410771461001e5780632096525514610024575f5ffd5b6004355f555b5f545f5260205ff3));
-)
+    bytes SimpleStoreBytecode = hex"602d8060093d393df35f3560e01c8063552410771461001e5780632096525514610024575f5ffd5b6004355f555b5f545f5260205ff3";
+
     /// @dev Setup the testing environment.
     function setUp() public {
         factory = Create3Factory(HuffDeployer.deploy("Create3Factory"));
@@ -34,9 +34,10 @@ contract Create3FactoryTest is Test {
     }
 
     /// @dev Ensure given contract deploys to the deterministic address
-    function testDeploy(bytes32 salt) public {
-        factory.deploy(salt, SimpleStoreBytecode);
-        address addr = factory.getDeterministicAddress(salt);
+    function testDeploy() public {
+        address addr = factory.getDeterministicAddress(hex"02");
+        require(addr.code.length == 0);
+        factory.deploy(hex"01", SimpleStoreBytecode);
         console.log(addr);
     }
     
